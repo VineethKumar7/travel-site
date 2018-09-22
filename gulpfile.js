c92eb72d87c925c3458f52551dde38497bdc489c
“VineethKumar7” <k.vineeth26@gmail.com>
2018-09-22 12:22:16 +0530

    Browser sinc

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import')
+browserSync = require('browser-sync').create();
 //task('task_name','what_will_happen_when_task_runs')
 gulp.task('default',function(){
    console.log("Hooray - you created a Gulp task");
@@ -24,12 +25,27 @@ gulp.task('watch',function(){
     // Is where we use the gulp watch plugin
     //We pass in two arguments watch('file',) 'file' is the file on the computer where it must save changes to.
     // './; will go to the root of the app folder
+
+    browserSync.init({
+      notify:false,
+      server: {
+        baseDir: "app"
+      }
+    });
     watch('./app/index.html', function(){
         // So when ever we make changes to html file we automatically tigger the tasks. Here we tigger the html tasks.
-        gulp.start('html');
+        browserSync.reload();
 
     });
     watch('./app/assets/styles/**/*.css', function(){
-        gulp.start('styles');
+        gulp.start('cssInject');
     });
+});
+gulp.task('cssInject',['styles'], function(){
+  //We want to take the contents of our compiled CSS file and hand that over to the browser sync, we can inject the CSS on the fly.
+  //The browserSync has a method named stream that will make whatever we are piping into it available in the browser.
+  //Since the gulp.src begins with the src() which is a sinc function, so it must begin with return.
+  gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+
 });
